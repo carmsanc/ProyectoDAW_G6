f8036cf1eb840264f6a9ecf3482fb7b3c13a6694
Nicole <ngalvare@hotmail.com>
2020-01-16 19:27:13 -0500

    Se crea token de authenticacion

--- djangoAdmin/startelpProject/startelpApp/urls.py
+++ djangoAdmin/startelpProject/startelpApp/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.conf.urls import url,include
-from django.urls import path
 from startelpApp import views
 from rest_framework import routers
-
+from rest_framework.authtoken.views import ObtainAuthToken
 
 router =routers.DefaultRouter()
 router.register(r'users',views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^auth/', ObtainAuthToken.as_view())       
 ]
--- djangoAdmin/startelpProject/startelpApp/views.py
+++ djangoAdmin/startelpProject/startelpApp/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from startelpApp.serializers import UserSerializer
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -9,3 +10,5 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
